0e07cbb217b129de0ef388de20867d7b49f094e0
xatify <abbouzid@student.1337.ma>
2022-05-19 21:13:22 +0100

    redesigning the data connection handle to state pattern

--- Networking/Connections/Connection.hpp
+++ Networking/Connections/Connection.hpp
@@ -3,26 +3,15 @@
 
 #include <string>
 
-enum State {
-	PARSING,
-	ERROR,
-	SENDING,
-	EXECUTE,
-	CLOSE
-};
-
 class Connection {
 	private:
 		std::string		type;
 		int				fd;
-		State			state;
 	public:
 		Connection (int fd, const std::string& _type);
 		virtual  ~Connection ();
 		const	std::string& getType () const;
 		int		getFd () const;
-		State	getState () const;
-		void	setState (State e);
 		void	close ();
 };
 
